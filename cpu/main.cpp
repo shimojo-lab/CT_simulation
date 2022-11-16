7f7bdbf95fd888e6cc383e0707e11300ce228cfc
sakimori-coder <morisaki444@gmail.com>
2022-11-16 14:07:25 +0900

    比較

--- cpu/main.cpp
+++ cpu/main.cpp
@@ -1,50 +1,71 @@
-#include <complex>
-#include <string>
-#include <math.h>
 #include <iostream>
+#include <fstream>
+#include <time.h>
 #include "state.hpp"
+#include "exist_state.hpp"
 
-int main()
-{
-    int n = 30;
+int main(){
+    int n = 25;
     QuantumState state(n);
+    exist_QuantumState exist_state(n);
+
     clock_t start, end;
-    double time;
+    double time, exist_time;
 
-    state.act_H(0);   
 
-    start = clock();
-    state.act_H(10);
-    end = clock();
-    time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000.0;
-    printf("H %lf[ms]\n", time);
+    std::ofstream H_ofs("H_gate.csv");
+    for(int i = 0; i < n; i++){
+        // 提案手法の時間計測
+        start = clock();
+        state.act_H(i);
+        end = clock();
+        time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000.0;
 
-    start = clock();
-    state.act_S(10);
-    end = clock();
-    time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000.0;
-    printf("S %lf[ms]\n", time);
+        // 既存手法の時間計測
+        start = clock();
+        exist_state.act_H(i);
+        end = clock();
+        exist_time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000.0;
 
-    start = clock();
-    state.act_T(10);
-    end = clock();
-    time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000.0;
-    printf("T %lf[ms]\n", time);
+        H_ofs << "H" << i << ", " << time << ", " << exist_time << ", "  << time / exist_time << ", "<< std::endl;
+    }
 
-    start = clock();
-    state.act_CNOT(4,8);
-    end = clock();
-    time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000.0;
-    printf("CNOT %lf[ms]\n", time);
+    std::ofstream S_ofs("S_gate.csv");
+    for(int i = 0; i < n; i++){
+        // 提案手法の時間計測
+        start = clock();
+        state.act_S(i);
+        end = clock();
+        time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000.0;
 
-    //for(int i = 0; i < 1; i++) state.act_H(i);  
-    // std::string str = "HTSHTSHTSHTHTHTHTSHTHTSHTSHTSHTHTHTSHTSHTHTHTSHTHTSHTHTHTHTHTHTHTSHTSHTSHTHTSHTHTSHTHTHTHTSHTHTHTSHTHTSHTHTHTHTSHTSHTSHTHTHTSHTSHTSHTSHTHTSHTSHTSHTSHTHTSHTHTSHTSHTHTHTHTHTSHTHTHTHTSHTSHTSHTHTSHTSHTHTHTSHTHTHTHTHTSHTSHTHTHTHTHTSHTHTHTHTSHTHTHTHTHTHTH";
-    // for(int i = 0; i < str.size(); i++){
-    //     if(str[i] == 'H') state.act_H(0);
-    //     else{
-    //         state.act_T(0);
-    //         if(str[i] == 'S') state.act_T(0);
-    //     }
-    // }
-    //std::cout << state << std::endl;
+        // 既存手法の時間計測
+        start = clock();
+        exist_state.act_S(i);
+        end = clock();
+        exist_time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000.0;
+
+        S_ofs << "S" << i << ", " << time << ", " << exist_time << ", "  << time / exist_time << ", "<< std::endl;
+    }
+
+    std::ofstream T_ofs("T_gate.csv");
+    for(int i = 0; i < n; i++){
+        // 提案手法の時間計測
+        start = clock();
+        state.act_T(i);
+        end = clock();
+        time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000.0;
+
+        // 既存手法の時間計測
+        start = clock();
+        exist_state.act_T(i);
+        end = clock();
+        exist_time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000.0;
+
+        T_ofs << "T" << i << ", " << time << ", " << exist_time << ", "  << time / exist_time << ", "<< std::endl;
+    }
+
+    CTYPE* vec = state.get_vec();
+    exist_CTYPE* vec_exist = exist_state.get_vec();
+    // std::cout << vec[0] << std::endl;
+    return 0;
 }
